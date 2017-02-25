4f9bf2b33e1a6e1ede0e2a768fdf560444769a51
Nick Snyder <nickdsnyder@gmail.com>
2017-02-24 22:39:11 -0800

    Expose ReferenceContext so Sourcegraph can exclude dec
    https://github.com/sourcegraph/sourcegraph/issues/4340

--- src/vs/editor/contrib/referenceSearch/common/referenceSearch.ts
+++ src/vs/editor/contrib/referenceSearch/common/referenceSearch.ts
@@ -9,16 +9,17 @@ import { onUnexpectedExternalError } from 'vs/base/common/errors';
 import { TPromise } from 'vs/base/common/winjs.base';
 import { IReadOnlyModel } from 'vs/editor/common/editorCommon';
 import { CommonEditorRegistry } from 'vs/editor/common/editorCommonExtensions';
-import { Location, ReferenceProviderRegistry } from 'vs/editor/common/modes';
+import { Location, ReferenceProviderRegistry, ReferenceContext } from 'vs/editor/common/modes';
 import { asWinJsPromise } from 'vs/base/common/async';
 import { Position } from 'vs/editor/common/core/position';
 
-export function provideReferences(model: IReadOnlyModel, position: Position, progress: (locations: Location[]) => void): TPromise<Location[]> {
+export function provideReferences(model: IReadOnlyModel, position: Position, progress: (locations: Location[]) => void, context?: ReferenceContext): TPromise<Location[]> {
 
 	// collect references from all providers
 	const promises = ReferenceProviderRegistry.ordered(model).map(provider => {
 		return asWinJsPromise((token) => {
-			return provider.provideReferences(model, position, { includeDeclaration: true }, token, progress);
+			const ctx = context || { includeDeclaration: true };
+			return provider.provideReferences(model, position, ctx, token, progress);
 		}).then(result => {
 			if (Array.isArray(result)) {
 				return <Location[]>result;
