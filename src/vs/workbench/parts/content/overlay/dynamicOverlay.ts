a6ee96755d1655f790707c10b506a67f8c84891c
Matthew King <kingy895@gmail.com>
2017-03-29 10:48:39 -0700

    Remove css file

--- src/vs/workbench/parts/content/overlay/dynamicOverlay.ts
+++ src/vs/workbench/parts/content/overlay/dynamicOverlay.ts
@@ -4,71 +4,84 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-import 'vs/css!./dynamicOverlay';
 import { $, Builder } from 'vs/base/browser/builder';
-import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
-import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
 import { Parts, IPartService } from 'vs/workbench/services/part/common/partService';
-import { ICommandService } from 'vs/platform/commands/common/commands';
-import { IDisposable, dispose } from 'vs/base/common/lifecycle';
 import { RawContextKey, IContextKey, IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 
-const OVERLAY_VISIBLE = new RawContextKey<boolean>('interfaceOverviewVisible', false);
+const OVERLAY_VISIBLE = new RawContextKey<boolean>('dynamicOverlayVisible', false);
 
 export class DynamicOverlay {
-
-	private _toDispose: IDisposable[] = [];
 	private _overlayVisible: IContextKey<boolean>;
 	private _overlay: Builder;
 
 	constructor(
 		@IPartService private partService: IPartService,
-		@IWorkbenchEditorService private editorService: IWorkbenchEditorService,
-		@ICommandService private commandService: ICommandService,
 		@IContextKeyService private _contextKeyService: IContextKeyService,
-		@IKeybindingService private keybindingService: IKeybindingService
 	) {
 		this._overlayVisible = OVERLAY_VISIBLE.bindTo(this._contextKeyService);
 	}
 
-	public create(content?: Builder, overlayStyles?: any): void {
-		const container = this.partService.getContainer(Parts.EDITOR_PART);
+	/**
+	 * Creates a customizable component to be rendered anywhere inside of the editor.
+	 * @param  {Builder} content A full builder object to be appended to the Overlay.
+	 * @param  {any} overlayStyles? Custom styling for the Overlay component otherwise default styles are used.
+	 * @param  {HTMLElement} parentContainer? The container to be wrapped around to provid a way for the element to manipulate it and add more child elements. Default value EDITOR_PART container.
+	 * @returns void
+	 */
+	public create(content: Builder, overlayStyles?: any, parentContainer?: HTMLElement): void {
+		const container = parentContainer ? parentContainer : this.partService.getContainer(Parts.EDITOR_PART);
 
-		const isStatusbarHidden = !this.partService.isVisible(Parts.STATUSBAR_PART);
-		const offset = isStatusbarHidden ? 0 : 32;
 		this._overlay = $(container.parentElement)
-			.div({ 'class': 'dynamicOverlay' })
-			.style(overlayStyles ? overlayStyles : { height: `calc(100% - ${offset}px)`, backgroundColor: 'white' })
+			.div({ 'class': 'dynamic-view-overlay' })
+			.style(overlayStyles ? overlayStyles : this.getDefaultOverlayStyles())
 			.display('none');
-		this._toDispose.push(this._overlay);
 
-		if (content) {
-			$(this._overlay).append(content);
-		}
+		$(this._overlay).append(content);
 	}
 
-	public show() {
+
+	/**
+	 * Default overlay styles to be used.
+	 * @returns any
+	 */
+	public getDefaultOverlayStyles(): any {
+		const offset = this.partService.getTitleBarOffset();
+		return { position: 'relative', height: `calc(100% - ${offset}px)`, width: '100%', zIndex: '1999', backgroundColor: 'white' };
+	}
+
+	/**
+	 * Sets the overlyVisible state to true and updates the overlay display value
+	 * @param  {string} display? CSS display value to be used when the overlay is shown. Default is block.
+	 */
+	public show(display?: string): void {
 		if (!this._overlay) {
 			console.error('Create must be called before calling show() dynamicOverlay');
 			return;
 		}
-		if (this._overlay.style('display') !== 'flex') {
-			this._overlay.display('flex');
+		if (this._overlay.style('display') === 'none') {
+			this._overlay.display(display || 'block');
 			this._overlayVisible.set(true);
 		}
 	}
 
-	public hide() {
-		this._overlay.display('none');
-		this._overlayVisible.reset();
+	/**
+	 * Sets the overlayVisible state to false and updates the overlay's display value to 'none'.
+	 * @returns void
+	 */
+	public hide(): void {
+		if (this._overlay && this._overlay.style('display') !== 'none') {
+			this._overlay.display('none');
+			this._overlayVisible.reset();
+		}
 	}
 
-	public destory() {
+	/**
+	 * Removes the current HTML element and all its children from its parent and unbinds
+	 * all listeners and properties set to the data slots.
+	 * @returns void
+	 */
+	public destory(): void {
 		this.hide();
 		this._overlay.destroy();
-	}
-
-	dispose() {
-		this._toDispose = dispose(this._toDispose);
 	}
 }
