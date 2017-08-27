0ab7a07a8d85fed500c987d72bfe9864b1f03174
Quinn Slack <qslack@qslack.com>
2017-08-27 15:18:09 -0700

    rm unused file

--- src/vs/platform/workspace/common/folder.ts
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Sourcegraph. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-'use strict';
-
-import URI from 'vs/base/common/uri';
-
