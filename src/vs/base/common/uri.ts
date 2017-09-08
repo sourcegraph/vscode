1a2816c59f524efbf0205ed99b0c7c4961574f01
Nick Snyder <nick@sourcegraph.com>
2017-09-08 15:15:15 -0700

    Revert "Handle user information in URI (#868)"
    This reverts commit d121bb222d3d6f2d84aa4a14bfe1fb5c219fabad.

--- src/vs/base/common/uri.ts
+++ src/vs/base/common/uri.ts
@@ -301,20 +301,8 @@ export default class URI {
 			parts.push('//');
 		}
 		if (authority) {
-			let idx = authority.indexOf('@');
-			if (idx !== -1) {
-				const userinfo = authority.substr(0, idx);
-				authority = authority.substr(idx + 1);
-				idx = userinfo.indexOf(':');
-				if (idx === -1) {
-					parts.push(encoder(userinfo));
-				} else {
-					parts.push(encoder(userinfo.substr(0, idx)), ':', encoder(userinfo.substr(idx + 1)));
-				}
-				parts.push('@');
-			}
 			authority = authority.toLowerCase();
-			idx = authority.indexOf(':');
+			let idx = authority.indexOf(':');
 			if (idx === -1) {
 				parts.push(encoder(authority));
 			} else {
