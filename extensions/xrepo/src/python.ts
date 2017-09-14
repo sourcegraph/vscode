68dc637f862224df7a1f7f834c7a54bc672d867a
Beyang Liu <beyang@sourcegraph.com>
2017-09-14 00:27:10 -0700

    Python go-to-source

---
+++ extensions/xrepo/src/python.ts
@@ -0,0 +1,77 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Sourcegraph. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { walk } from './util';
+
+/**
+ * Package descriptor
+ */
+interface PackageInfo {}
+
+interface DefinitionInfo extends PackageInfo {
+	modulePath: string;
+	selection: vscode.Selection;
+}
+
+export async function getSourceLocation(uri: vscode.Uri, selection: vscode.Selection): Promise<vscode.Location[]> {
+	const finfo = await definitionInfo(uri, selection);
+	return finfo ? getDefSourceLocation(finfo) : [];
+}
+
+async function getDefSourceLocation(fileInfo: DefinitionInfo): Promise<vscode.Location[]> {
+	if (!vscode.workspace.workspaceFolders) {
+		return [];
+	}
+
+	const found: vscode.Uri[] = [];
+	const matches: vscode.Location[][] = await Promise.all(
+		vscode.workspace.workspaceFolders.map(wsFolder => findDefinition(wsFolder, fileInfo))
+	);
+	const flatMatches: vscode.Location[] = [];
+	for (const wsMatches of matches) {
+		flatMatches.push(...wsMatches);
+	}
+	return flatMatches;
+}
+
+async function findDefinition(workspaceFolder: vscode.WorkspaceFolder, defInfo: DefinitionInfo): Promise<vscode.Location[]> {
+	const moduleBasename = path.basename(defInfo.modulePath);
+	const matches: string[] = [];
+	await walk(workspaceFolder.uri.fsPath, (filepath, stats) => {
+		if (path.basename(filepath) === 'site-packages' && stats.isDirectory()) {
+			return false;
+		}
+		if (path.basename(filepath) === moduleBasename && stats.isFile()) {
+			if (filepath.endsWith(defInfo.modulePath)) {
+				matches.push(filepath);
+			}
+		}
+		return true;
+	});
+	return matches.map(m => new vscode.Location(vscode.Uri.file(m), defInfo.selection));
+}
+
+/**
+ * DRAFT: returns null when file is not an installed dependency
+ * uses simple filesystem heuristics (site-packages); test on both virtualenv and system path
+ */
+async function definitionInfo(uri: vscode.Uri, selection: vscode.Selection): Promise<DefinitionInfo | null> {
+	const fsPath = uri.fsPath;
+	const i = fsPath.indexOf('site-packages/');
+	if (i === -1) {
+		return null;
+	}
+	const remainder = fsPath.substr(i + 'site-packages/'.length);
+	let relpathCmps = remainder.split(path.sep);
+	if (relpathCmps[0].endsWith('.egg')) {
+		relpathCmps = relpathCmps.slice(1);
+	}
+	const relpath = relpathCmps.join(path.sep);
+	return { modulePath: relpath, selection: selection };
+}
--- extensions/xrepo/src/util.ts
+++ extensions/xrepo/src/util.ts
@@ -8,7 +8,7 @@
 'use strict';
 
 import * as fs from 'fs';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 
 export function nfcall<R>(fn: Function, ...args: any[]): Promise<R> {
 	return new Promise<R>((c, e) => fn(...args, (err: any, r: any) => err ? e(err) : c(r)));
@@ -50,4 +50,20 @@ export async function mkdirp(path: string, mode?: number): Promise<boolean> {
 	}
 
 	return true;
+}
+
+export async function walk(root: string, visit: (filepath: string, stats: fs.Stats) => boolean): Promise<void> {
+	const stats = await new Promise<fs.Stats>((resolve, reject) => fs.lstat(root, (err, stats) => err ? reject(err) : resolve(stats)));
+	const descend = visit(root, stats);
+	if (!descend) {
+		return;
+	}
+	if (stats.isSymbolicLink()) {
+		return;
+	}
+	if (!stats.isDirectory()) {
+		return;
+	}
+	const children = await new Promise<string[]>((resolve, reject) => fs.readdir(root, (err, files) => err ? reject(err) : resolve(files)));
+	await Promise.all(children.map(c => walk(join(root, c), visit)));
 }
