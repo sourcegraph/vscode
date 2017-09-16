3c058cd2f0c09d83fd72dfe23185291af9f7dc4f
Beyang Liu <beyang@sourcegraph.com>
2017-09-15 17:20:50 -0700

    Typescript go-to-source

--- extensions/xrepo/src/typescript.ts
+++ extensions/xrepo/src/typescript.ts
@@ -6,6 +6,8 @@
 
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { readFile } from './nodeutil';
+import { walk } from './util';
 
 interface PackageInfo {
 	package: string;
@@ -17,19 +19,83 @@ interface DefinitionInfo extends PackageInfo {
 	selection: vscode.Selection;
 }
 
-
 export async function getSourceLocation(uri: vscode.Uri, selection: vscode.Selection): Promise<vscode.Location[]> {
-	return [];
+	const defInfo = await definitionInfo(uri, selection);
+	return defInfo ? getDefSourceLocation(defInfo) : [];
 }
 
 async function getDefSourceLocation(defInfo: DefinitionInfo): Promise<vscode.Location[]> {
-	return []
+	if (!vscode.workspace.workspaceFolders) {
+		return [];
+	}
+
+	const matches = await Promise.all(
+		vscode.workspace.workspaceFolders.map(wsFolder => findDefinition(wsFolder, defInfo))
+	);
+	const flatMatches = [];
+	for (const wsMatches of matches) {
+		flatMatches.push(...wsMatches);
+	}
+	return flatMatches;
 }
 
-async function defInfo(uri: vscode.Uri, selection: vscode.Selection): Promise<DefinitionInfo | null> {
-	return null;
+async function definitionInfo(uri: vscode.Uri, selection: vscode.Selection): Promise<DefinitionInfo | null> {
+	const fsPathCmps = uri.fsPath.split(path.sep);
+	const i = fsPathCmps.lastIndexOf('node_modules');
+	if (i === -1) {
+		return null;
+	}
+	let relCmps = fsPathCmps.slice(i + 1);
+	if (relCmps.length > 0 && relCmps[0] === '@types') {
+		relCmps = relCmps.slice(1);
+	}
+	if (relCmps.length < 2) {
+		return null;
+	}
+	const [pkgName, ...filePathCmps] = relCmps;
+	return {
+		package: pkgName,
+		selection: selection,
+		filePath: filePathCmps.join(path.sep),
+	};
 }
 
 async function findDefinition(workspaceFolder: vscode.WorkspaceFolder, defInfo: DefinitionInfo): Promise<vscode.Location[]> {
-	return [];
+	// Find package.json files
+	const pkgJsonFiles: string[] = [];
+	await walk(workspaceFolder.uri.fsPath, (filepath, stats) => {
+		const basename = path.basename(filepath);
+		if (basename === 'node_modules' && stats.isDirectory()) {
+			return false;
+		}
+		if (basename === 'package.json' && stats.isFile()) {
+			pkgJsonFiles.push(filepath);
+		}
+		return true;
+	});
+
+	const locsByPkg: vscode.Location[][] = await Promise.all(pkgJsonFiles.map(f => findDefinitionInPackage(f, defInfo)));
+	const locs = [];
+	for (const l of locsByPkg) {
+		locs.push(...l);
+	}
+	return locs;
+}
+
+/**
+ * findDefinitionInPackage returns the location(s) of the definition in the package defined by the specifice package.json.
+ * Currently, this just returns the location of the package.json file if the definition's package name matches the
+ * package defined.
+ */
+async function findDefinitionInPackage(packageJsonFile: string, defInfo: DefinitionInfo): Promise<vscode.Location[]> {
+	let metadata;
+	try {
+		metadata = JSON.parse(await readFile(packageJsonFile, 'utf8'));
+	} catch {
+		return [];
+	}
+	if (metadata.name !== defInfo.package) {
+		return [];
+	}
+	return [new vscode.Location(vscode.Uri.file(packageJsonFile), new vscode.Range(0, 0, 0, 0))];
 }
