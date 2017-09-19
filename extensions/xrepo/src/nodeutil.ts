2f73b95d132f5f85f6ba7103e91cd6b1b5ea9b6b
Beyang Liu <beyang@sourcegraph.com>
2017-09-19 12:05:17 -0700

    xrepo: node convenience functions

--- extensions/xrepo/src/nodeutil.ts
+++ extensions/xrepo/src/nodeutil.ts
@@ -6,7 +6,16 @@
 'use strict';
 
 import * as fs from 'fs';
+import * as cp from 'child_process';
 
 export function readFile(filename: string, encoding: string): Promise<string> {
 	return new Promise<string>((resolve, reject) => fs.readFile(filename, encoding, (err, data) => err ? reject(err) : resolve(data)));
+}
+
+export function execFile(file: string, args?: string[], options?: cp.ExecFileOptions): Promise<[string, string]> {
+	return new Promise((resolve, reject) => cp.execFile(file, args, options, (err, stdout, stderr) => err ? reject(err) : resolve([stdout, stderr])));
+}
+
+export function pathExists(filename: string): Promise<boolean> {
+	return new Promise(resolve => fs.access(filename, fs.constants.F_OK, err => err ? resolve(false) : resolve(true)));
 }
