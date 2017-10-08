371b0f1f794b782216a17c78e2802c8f25a17368
M. Hofman <m.hofman@chronos-technology.nl>
2017-10-08 16:23:05 +0200

    Retrieve repositories from GitLab

--- extensions/service-gitlab/src/GitlabViewer.ts
+++ extensions/service-gitlab/src/GitlabViewer.ts
@@ -1,80 +1,177 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Sourcegraph. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import * as vscode from 'vscode';
+
+const GITLAB_SCHEME = 'gitlab';
 
 // Gets Gitlab information associated for the current user. Uses a cache to prevent multiple
 // requests. Based on the github viewer.
 export class GitlabViewer {
-    private token: string;
-    private host: string;
+	private token: string;
+	private host: string;
+	private userid: number;
+
 	private repoRequest: Thenable<vscode.CatalogFolder[]> | null;
 	private usernameRequest: Thenable<string | null> | null;
+	private userIdRequest: Thenable<number | null>;
 
 	// Returns the username of the currently logged in user. It is best-effort, so if the
 	// network request fails or there is no logged in user null is returned.
 	public username(): Thenable<string | null> {
 		if (!this.validState()) {
 			return Promise.resolve(null);
-        }
-        
+		}
+
 		if (this.usernameRequest !== null) {
 			return this.usernameRequest;
-        }
-        
-        const request = this.doGitlabRequest(this.host, this.token, '/user')
-            
-        .then<string | null>((data: any) => {
+		}
+
+		const request = this.doGitlabRequest(this.host, this.token, '/user')
+
+			.then<string | null>((data: any) => {
 				return data.viewer.login;
 			}, (reason) => {
 				// try again, but don't fail other requests if this fails
 				console.error(reason);
 				this.usernameRequest = null;
 				return null;
-            });
-            
+			});
+
 		this.usernameRequest = request;
+		return request;
+	}
+
+	// Returns the userid of the currently logged in user. It is best-effort, so if the
+	// network request fails or there is no logged in user null is returned.
+	public userId(): Thenable<number | null> {
+		if (!this.validState()) {
+			return Promise.resolve(null);
+		}
+
+		if (this.usernameRequest !== null) {
+			return this.userIdRequest;
+		}
+
+		const request = this.doGitlabRequest(this.host, this.token, '/user')
+			.then<number | null>((data: any) => {
+				return parseInt(data.viewer.id, 10);
+			}, (reason) => {
+				// try again, but don't fail other requests if this fails
+				console.error(reason);
+				this.usernameRequest = null;
+				return null;
+			});
+
+		this.userIdRequest = request;
+		return request;
+	}
+
+	// Retrieve Gitlab Repositories for the current user.
+	// 
+	// Like the github this one is best-effort, so rejections should never happen. Uses
+	// internal cache for efficiency.
+	public repositories(): Thenable<vscode.CatalogFolder[]> {
+		if (!this.validState()) {
+			return Promise.resolve([]);
+		}
+
+		if (this.repoRequest !== null) {
+			return this.repoRequest;
+		}
+
+		const url = `/users/${this.userid}/projects`;
+
+		const request = this.doGitlabRequest(this.host, this.token, url)
+			.then<vscode.CatalogFolder[]>((data: any[]) => {
+				return data.map(this.toCatalogFolder);
+			},
+			(reason) => {
+				console.error(reason);
+				this.usernameRequest = null;
+				return null;
+			});
+
 		return request;
 	}
 
 	// Returns true if you can do a request or use a cached request.
 	private validState(): boolean {
-        const token = vscode.workspace.getConfiguration('gitlab').get<string>('token');
-        const host = vscode.workspace.getConfiguration('gitlab').get<string>('host');
+		const token = vscode.workspace.getConfiguration('gitlab').get<string>('token');
+		const host = vscode.workspace.getConfiguration('gitlab').get<string>('host');
+		const userid = vscode.workspace.getConfiguration('gitlab').get<number>('userid');
+
 		if (!token) {
 			return false;
-        }
+		}
 
-        if(!host) {
-            return false;
-        }
+		if (!host) {
+			return false;
+		}
+
+		if (!userid) {
+			return false;
+		}
 
 		if (token !== this.token) {
 			this.repoRequest = null;
 			this.usernameRequest = null;
 		}
-        
-        if(host !== this.host) {
-            this.repoRequest = null;
-            this.usernameRequest = null;
-        }
 
-        this.token = token;
-        this.host = host;
+		if (host !== this.host) {
+			this.repoRequest = null;
+			this.usernameRequest = null;
+		}
+
+		if (userid !== this.userid) {
+			this.repoRequest = null;
+			this.usernameRequest = null;
+		}
+
+		this.token = token;
+		this.host = host;
+		this.userid = userid;
 
 		return true;
-    }
-    
-    private doGitlabRequest<T>(host: string, token: string, endpoint: string): Thenable<T> {
-        return fetch(`${host}/api/v4${endpoint}?private_toke=${token}`,{
-            method: 'GET'
-        })
-        .then(response => {
-            if( response.status < 200 || response.status > 299) {
-                return response.json().then(
-                    // TODO: Put in error message
-                    (error: { message: string} ) => {}
-                );
-            }
+	}
 
-            return response.json();
-        });
-    }
+	private doGitlabRequest<T>(host: string, token: string, endpoint: string): Thenable<T> {
+		return fetch(`${host}/api/v4${endpoint}?private_toke=${token}`, {
+			method: 'GET'
+		})
+			.then(response => {
+				if (response.status < 200 || response.status > 299) {
+					return response.json().then(
+						// TODO: Put in error message
+						(error: { message: string }) => { }
+					);
+				}
+
+				return response.json();
+			});
+	}
+
+	private toCatalogFolder(repository: any): vscode.CatalogFolder {
+		return {
+			resource: vscode.Uri.parse('').with({ scheme: GITLAB_SCHEME, authority: this.host, path: `/repository/${repository.path_with_namespace}` }),
+			displayPath: repository.repository.path_with_namespace,
+			displayName: repository.name,
+			genericIconClass: this.iconForRepo(repository),
+			cloneUrl: vscode.Uri.parse('').with({ scheme: 'https', authority: this.host, path: `/${repository.path_with_namespace}.git` }),
+			isPrivate: repository.visibility === 'private',
+			starsCount: repository.star_count,
+			forksCount: repository.forks_count,
+			description: repository.description,
+			createdAt: new Date(Date.parse(repository.created_at)),
+			updatedAt: repository.last_activity_at ? new Date(Date.parse(repository.last_activity_at)) : undefined,
+		};
+	}
+
+	private iconForRepo(repo: { visibility: string }) {
+		if (repo.visibility === 'private') {
+			return 'lock';
+		}
+		return 'repo';
+	}
 }
--- extensions/service-gitlab/src/main.ts
+++ extensions/service-gitlab/src/main.ts
@@ -6,25 +6,28 @@
 
 import * as vscode from 'vscode';
 import * as nls from 'vscode-nls';
+import { GitlabViewer } from './GitlabViewer';
 
 const localize = nls.loadMessageBundle();
 
-const GITLAB_SCHEME = 'gitlab';
+//const GITLAB_SCHEME = 'gitlab';
 
 export function activate(context: vscode.ExtensionContext): void {
+	const viewer = new GitlabViewer();
+
 	vscode.commands.registerCommand('gitlab.checkAccessToken', async (args) => {
 		return checkgitlabToken();
 	});
 
 	vscode.commands.registerCommand('gitlab.showCreateAccessTokenWalkthrough', async (skipInfoMessage) => {
-		return await showCreategitlabTokenWalkthrough(skipInfoMessage);
+		return await showCreategitlabTokenWalkthrough(viewer, skipInfoMessage);
 	});
 }
 
 /**
  * Shows the gitlab token creation walkthrough and returns if a gitlab token was added.
  */
-async function showCreategitlabTokenWalkthrough(skipInfoMessage?: boolean): Promise<boolean> {
+async function showCreategitlabTokenWalkthrough(viewer: GitlabViewer, skipInfoMessage?: boolean): Promise<boolean> {
 	// Close quickopen so the user sees our message.
 	await vscode.commands.executeCommand('workbench.action.closeQuickOpen');
 	await vscode.commands.executeCommand('workbench.action.closeMessages');
@@ -32,7 +35,7 @@ async function showCreategitlabTokenWalkthrough(skipInfoMessage?: boolean): Prom
 	const createTokenItem: vscode.MessageItem = { title: localize('createToken', "Create Token on gitlab.com") };
 	const enterTokenItem: vscode.MessageItem = { title: localize('enterToken', "Enter Token") };
 	const cancelItem: vscode.MessageItem = { title: localize('cancel', "Cancel"), isCloseAffordance: true };
-	
+
 	const enterHostItem: vscode.MessageItem = { title: localize('enterHost', "Enter host name.") };
 
 	const value = await vscode.window.showInformationMessage(
@@ -40,7 +43,7 @@ async function showCreategitlabTokenWalkthrough(skipInfoMessage?: boolean): Prom
 		{ modal: false },
 		enterHostItem, cancelItem,
 	);
-	
+
 	// Display host message
 	let host;
 	if (value === enterHostItem) {
@@ -48,14 +51,14 @@ async function showCreategitlabTokenWalkthrough(skipInfoMessage?: boolean): Prom
 			prompt: localize('hostPrompt', "GitLab host is required to search for repositories (defaults to gitlab.com)"),
 			ignoreFocusOut: true,
 		});
-		
-		if(host === undefined) {
+
+		if (host === undefined) {
 			host = 'gitlab.com';
 		}
 	} else if (!value || value === cancelItem) {
 		return false;
 	}
-	
+
 	if (host) {
 		await vscode.workspace.getConfiguration('gitlab').update('host', host, vscode.ConfigurationTarget.Global);
 	} else {
@@ -85,6 +88,16 @@ async function showCreategitlabTokenWalkthrough(skipInfoMessage?: boolean): Prom
 	});
 	if (token) {
 		await vscode.workspace.getConfiguration('gitlab').update('token', token, vscode.ConfigurationTarget.Global);
+
+		const userid = await viewer.userId();
+
+		if (userid === null) {
+			showErrorImmediately(localize('noUser', "Unable to retrieve user from GitLab."), viewer);
+			return false;
+		}
+
+		await vscode.workspace.getConfiguration('gitlab').update('userid', userid, vscode.ConfigurationTarget.Global);
+
 		return true;
 	}
 	return false;
@@ -95,4 +108,30 @@ async function showCreategitlabTokenWalkthrough(skipInfoMessage?: boolean): Prom
  */
 function checkgitlabToken(): boolean {
 	return !!vscode.workspace.getConfiguration('gitlab').get<string>('token');
+}
+
+/**
+ * Close quickopen and pass along the error so that the user sees it immediately instead
+ * of only when they close the quickopen (which probably isn't showing any results because of
+ * the error).
+ */
+function showErrorImmediately<T>(error: string, viewer: GitlabViewer): T | Thenable<T> {
+	return vscode.commands.executeCommand('workbench.action.closeQuickOpen').then(() => vscode.commands.executeCommand('workbench.action.closeMessages').then(() => {
+		const resetTokenItem: vscode.MessageItem = { title: localize('resetToken', "Reset Token") };
+		const cancelItem: vscode.MessageItem = { title: localize('cancel', "Cancel"), isCloseAffordance: true };
+		vscode.window.showErrorMessage(error, resetTokenItem, cancelItem)
+			.then(async (value) => {
+				if (value === resetTokenItem) {
+					const hasToken = vscode.workspace.getConfiguration('github').get<string>('token');
+					if (hasToken) {
+						await vscode.workspace.getConfiguration('github').update('token', undefined, vscode.ConfigurationTarget.Global);
+					}
+					if (checkgitlabToken()) {
+						showCreategitlabTokenWalkthrough(viewer); // will walk the user through recreating the token
+					}
+				}
+			});
+
+		return Promise.reject(error);
+	}));
 }
