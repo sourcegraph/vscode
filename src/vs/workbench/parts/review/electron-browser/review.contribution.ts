cff9ce825d6abfea181afab9064120eb4bf45415
Felix Becker <felix.b@outlook.com>
2017-10-23 15:17:19 -0700

    Register Action for review quick open

--- src/vs/workbench/parts/review/electron-browser/review.contribution.ts
+++ src/vs/workbench/parts/review/electron-browser/review.contribution.ts
@@ -15,8 +15,22 @@ import { Registry } from 'vs/platform/registry/common/platform';
 // import { IViewletService } from 'vs/workbench/services/viewlet/browser/viewlet';
 // import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
 // import { ReviewViewlet } from 'vs/workbench/parts/review/electron-browser/reviewViewlet';
-import { IQuickOpenRegistry, Extensions as QuickOpenExtensions, QuickOpenHandlerDescriptor } from 'vs/workbench/browser/quickopen';
+import { IQuickOpenRegistry, Extensions as QuickOpenExtensions, QuickOpenHandlerDescriptor, QuickOpenAction } from 'vs/workbench/browser/quickopen';
 import { ReviewQuickOpenHandler } from 'vs/workbench/parts/review/electron-browser/reviewQuickOpenHandler';
+import { IWorkbenchActionRegistry, Extensions as ActionExtensions } from 'vs/workbench/common/actions';
+import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
+import { IQuickOpenService } from 'vs/platform/quickOpen/common/quickOpen';
+
+class ReviewQuickOpenAction extends QuickOpenAction {
+	public static readonly ID = 'workbench.action.review';
+	public static readonly LABEL = localize('reviewItems', "Review Code");
+
+	constructor(actionId: string, actionLabel: string, @IQuickOpenService quickOpenService: IQuickOpenService) {
+		super(actionId, actionLabel, ReviewQuickOpenHandler.PREFIX, quickOpenService);
+	}
+}
+const actionRegistry = Registry.as<IWorkbenchActionRegistry>(ActionExtensions.WorkbenchActions);
+actionRegistry.registerWorkbenchAction(new SyncActionDescriptor(ReviewQuickOpenAction, ReviewQuickOpenAction.ID, ReviewQuickOpenAction.LABEL), 'Review Code');
 
 // Register Quick Open
 (<IQuickOpenRegistry>Registry.as(QuickOpenExtensions.Quickopen)).registerQuickOpenHandler(
